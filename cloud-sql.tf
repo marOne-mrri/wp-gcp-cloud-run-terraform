6c709e45220ed01020fd65fbebda8bf1bd7cb063
marouane_ourdou <marouane_ourdou@carrefour.com>
2024-12-26 22:39:12 +0100

    update: db instance config

--- cloud-sql.tf
+++ cloud-sql.tf
@@ -1,8 +1,8 @@
 resource "google_sql_database_instance" "wp_db_instance" {
-  name             = "my-database-instance"
+  name             = "wp-db-instance"
   region           = "us-central1"
   database_version = "MYSQL_8_0"
-
+  depends_on = [google_service_networking_connection.private_vpc_connection]
   settings {
     tier              = "db-f1-micro"
     availability_type = "ZONAL"
@@ -16,7 +16,7 @@ resource "google_sql_database_instance" "wp_db_instance" {
 
     ip_configuration {
       ipv4_enabled                                  = false
-      private_network                               = data.google_compute_network.my-network.self_link
+      private_network                               = data.google_compute_network.my_network.self_link
       enable_private_path_for_google_cloud_services = true
     }
   }
@@ -35,6 +35,24 @@ resource "google_sql_user" "wp_db_user" {
   password = "changeme"
 }
 
-data "google_compute_network" "my-network" {
+data "google_compute_network" "my_network" {
   name = "default"
+}
+
+resource "google_compute_global_address" "private_ip_address" {
+  provider = google-beta
+
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = data.google_compute_network.my_network.id
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  provider = google-beta
+
+  network                 = data.google_compute_network.my_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
